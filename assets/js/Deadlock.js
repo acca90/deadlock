951763b3b7d04856eeb7c2b124dee179dcd090b0
Matheus Hernandes <105786@UPF.BR>
2018-09-14 16:04:03 -0300

    first of all

---
+++ assets/js/Deadlock.js
@@ -0,0 +1,14 @@
+const Deadlock = (function () {
+    /**
+     * Inicializa simulação de deadlock
+     */
+    const init = function () {
+        document.getElementById('html').innerHTML = "it's work";
+    };
+    /**
+     * Métodos públicos
+     */
+    return {
+        init: init
+    };
+})();
