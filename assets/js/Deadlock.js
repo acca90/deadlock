b1a0f96151e234d3bb4068c2a351f36311d64e09
Matheus Hernandes <105786@UPF.BR>
2018-09-14 16:35:09 -0300

    first of all

--- assets/js/Deadlock.js
+++ assets/js/Deadlock.js
@@ -1,9 +1,19 @@
 const Deadlock = (function () {
     /**
+     * Controlador do canvas
+     */
+    const areaCanvas = {
+        canvas: document.getElementById('thadSimulationGaveMeHeadache'),
+        iniciar: function () {
+            this.context = this.canvas.getContext("2d");
+        }
+    };
+    /**
      * Inicializa simulação de deadlock
      */
     const init = function () {
-        document.getElementById('html').innerHTML = "it's work";
+        let component = new Component(30,30,'red',100,100);
+        areaCanvas.iniciar();
     };
     /**
      * Métodos públicos
